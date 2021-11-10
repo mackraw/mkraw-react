8bef2d3dad94f36b329b9607a40d128dd498ff08
mackraw <maciekrawczyk@gmail.com>
2021-11-10 11:03:13 -0600

    removed unneeded node

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Nav />
       <Hero />
       <Work />
       <About />
       <Contact />
       <Footer />
-    </div>
+    </>
   );
 }
 
